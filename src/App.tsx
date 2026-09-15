34d6f299041f135f2f4c0254dc12675b2c5d815d
Juan Rodriguez <juanblud@udel.edu>
2026-09-15 16:45:42 -0400

    staged all changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,30 @@
 import React from "react";
 import "./App.css";
-import { Button, Col, Container, Row } from "react-bootstrap";
+import { ChangeType } from "./components/ChangeType";
+//import { RevealAnswer } from "./components/RevealAnswer";
+//import { StartAttempt } from "./components/StartAttempt";
+//import { TwoDice } from "./components/TwoDice";
+//import { CycleHoliday } from "./components/CycleHoliday";
+//import { Counter } from "./components/Counter";
 
 function App(): React.JSX.Element {
-    function logHelloWorld(): void {
-        console.log("Hello World!");
-    }
     return (
         <div className="App">
             <header className="App-header">
                 <h1>UD CISC275 with React Hooks and TypeScript</h1>
             </header>
-            <p className="StudentName">Juan Rodriguez</p>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
-            <p>Hello World</p>
-            <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4kIhv15XaX_PtNkKwvNBIOsHPUn9AMydkYITPE86hLQ&s"
-                alt="Kuromi"
-            />
-            <ul>
-                <li>Pizza</li>
-                <li>Pasta</li>
-                <li>Alfredo</li>
-            </ul>
-            <Button onClick={logHelloWorld}>Log Hello World</Button>
-
-            <Container>
-                <Row>
-                    <Col>
-                        <div
-                            style={{
-                                width: "100px",
-                                height: "100px",
-                                backgroundColor: "red",
-                            }}
-                        />
-                    </Col>
-
-                    <Col>
-                        <div
-                            style={{
-                                width: "100px",
-                                height: "100px",
-                                backgroundColor: "red",
-                            }}
-                        />
-                    </Col>
-                </Row>
-            </Container>
+            <hr></hr>
+            <Counter></Counter>
+            <hr />
+            <RevealAnswer></RevealAnswer>
+            <hr />
+            <StartAttempt></StartAttempt>
+            <hr />
+            <TwoDice></TwoDice>
+            <hr />
+            <ChangeType></ChangeType>
+            <hr />
+            <CycleHoliday></CycleHoliday>
         </div>
     );
 }
