b20cc000278528022f17a3a04549de7080c25a7d
Juan Rodriguez <juanblud@udel.edu>
2026-09-15 21:41:30 -0400

    finished startattempt

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
-//import { StartAttempt } from "./components/StartAttempt";
+import { StartAttempt } from "./components/StartAttempt";
 //import { TwoDice } from "./components/TwoDice";
 //import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
@@ -18,9 +18,9 @@ function App(): React.JSX.Element {
             <hr />
             <RevealAnswer></RevealAnswer>
             <hr />
-            {/* <StartAttempt></StartAttempt>
+            <StartAttempt></StartAttempt>
             <hr />
-            <TwoDice></TwoDice>
+            {/* <TwoDice></TwoDice>
             <hr /> */}
             <ChangeType></ChangeType>
             <hr />
--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,39 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): React.JSX.Element {
-    return <div></div>;
+    const [attempts, setAttempts] = useState<number>(4);
+    const [inProgress, setInProgress] = useState<boolean>(false);
+
+    return (
+        <div>
+            <Button
+                onClick={() => {
+                    setAttempts(attempts - 1);
+                    setInProgress(true);
+                }}
+                disabled={inProgress || attempts === 0}
+            >
+                Start Quiz
+            </Button>
+
+            <Button
+                onClick={() => {
+                    setInProgress(false);
+                }}
+                disabled={!inProgress}
+            >
+                Stop Quiz
+            </Button>
+
+            <Button
+                onClick={() => {
+                    setAttempts(attempts + 1);
+                }}
+                disabled={inProgress}
+            >
+                Mulligan
+            </Button>
+            <p>Attempts Left: {attempts}</p>
+        </div>
+    );
 }
