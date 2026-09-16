4b9c918fe599f43b7e122e4bb3df49e4996bc242
Juan Rodriguez <juanblud@udel.edu>
2026-09-15 20:29:28 -0400

    Finished and passed the tests for counter, revealanswer, and changetype.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
 import { ChangeType } from "./components/ChangeType";
-//import { RevealAnswer } from "./components/RevealAnswer";
+import { RevealAnswer } from "./components/RevealAnswer";
 //import { StartAttempt } from "./components/StartAttempt";
 //import { TwoDice } from "./components/TwoDice";
 //import { CycleHoliday } from "./components/CycleHoliday";
-//import { Counter } from "./components/Counter";
+import { Counter } from "./components/Counter";
 
 function App(): React.JSX.Element {
     return (
@@ -18,13 +18,13 @@ function App(): React.JSX.Element {
             <hr />
             <RevealAnswer></RevealAnswer>
             <hr />
-            <StartAttempt></StartAttempt>
+            {/* <StartAttempt></StartAttempt>
             <hr />
             <TwoDice></TwoDice>
-            <hr />
+            <hr /> */}
             <ChangeType></ChangeType>
             <hr />
-            <CycleHoliday></CycleHoliday>
+            {/* <CycleHoliday></CycleHoliday> */}
         </div>
     );
 }
--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,18 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): React.JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [visible, setVisble] = useState<boolean>(false);
+    return (
+        <div>
+            <Button
+                onClick={() => {
+                    setVisble(!visible);
+                }}
+            >
+                Reveal Answer
+            </Button>
+
+            {visible && <span>42</span>}
+        </div>
+    );
 }
