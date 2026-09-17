570394dd1cf99569af6314650cc326f77fb2dd52
Juan Rodriguez <juanblud@udel.edu>
2026-09-17 13:16:05 -0400

    Finished my cycleholidays component

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,90 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+type Holiday =
+    | "Valentines"
+    | "Christmas"
+    | "Thanksgiving"
+    | "Halloween"
+    | "New Years";
+
 export function CycleHoliday(): React.JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [currentHoliday, setHoliday] = useState<Holiday>("Christmas");
+    function nextInYear(current: Holiday): Holiday {
+        if (current === "Valentines") {
+            return "Halloween";
+        }
+
+        if (current === "Halloween") {
+            return "Thanksgiving";
+        }
+        if (current === "Thanksgiving") {
+            return "Christmas";
+        }
+
+        if (current === "Christmas") {
+            return "New Years";
+        }
+
+        return "Valentines";
+    }
+
+    function nextAlphabetically(current: Holiday): Holiday {
+        if (current === "Valentines") {
+            return "Christmas";
+        }
+        if (current === "Christmas") {
+            return "Halloween";
+        }
+        if (current === "Halloween") {
+            return "New Years";
+        }
+
+        if (current === "New Years") {
+            return "Thanksgiving";
+        }
+
+        return "Valentines";
+    }
+
+    function setEmoji(current: Holiday): string {
+        if (current === "Valentines") {
+            return "💖";
+        }
+
+        if (current === "Christmas") {
+            return "🎅";
+        }
+
+        if (current === "Thanksgiving") {
+            return "🦃";
+        }
+
+        if (current === "Halloween") {
+            return "👻";
+        }
+
+        return "🥳";
+    }
+
+    return (
+        <div>
+            <p>Cycle Through Holidays</p>
+            <Button
+                onClick={() => {
+                    setHoliday(nextAlphabetically(currentHoliday));
+                }}
+            >
+                Advance by Alphabetical Order
+            </Button>
+            <Button
+                onClick={() => {
+                    setHoliday(nextInYear(currentHoliday));
+                }}
+            >
+                Advance by Order in Year
+            </Button>
+            <p>Current Holiday: {setEmoji(currentHoliday)}</p>
+        </div>
+    );
 }
