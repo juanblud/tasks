6316e010e1be365dc61f25752e5b42d688b7909e
Juan Rodriguez <juanblud@udel.edu>
2026-09-17 17:38:31 -0400

    fixed  all the bad components

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
 import { DoubleHalf } from "./bad-components/DoubleHalf";
 import { ColoredBox } from "./bad-components/ColoredBox";
-// import { ShoveBox } from "./bad-components/ShoveBox";
+import { ShoveBox } from "./bad-components/ShoveBox";
 import { ChooseTeam } from "./bad-components/ChooseTeam";
 
 function App(): React.JSX.Element {
@@ -25,8 +25,8 @@ function App(): React.JSX.Element {
             <hr></hr>
             <ColoredBox></ColoredBox>
             <hr></hr>
-            {/* <ShoveBox></ShoveBox>
-            <hr></hr> */}
+            <ShoveBox></ShoveBox>
+            <hr></hr>
             <Counter></Counter>
             <hr />
             <RevealAnswer></RevealAnswer>
