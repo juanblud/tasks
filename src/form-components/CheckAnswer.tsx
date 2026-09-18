853d2b5545ccf47c7d079725cbaaf147eb8ae041
Juan Rodriguez <juanblud@udel.edu>
2026-09-18 16:39:14 -0400

    I got the CheckAnswer form component working.

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -5,9 +5,16 @@ export function CheckAnswer({
 }: {
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [response, setResponse] = useState<string>("");
+
+    function updateResponse(event: React.ChangeEvent<HTMLInputElement>) {
+        setResponse(event.target.value);
+    }
+
     return (
         <div>
             <h3>Check Answer</h3>
+            <Form.Group controlId="checkAnswer"></Form.Group>
         </div>
     );
 }
