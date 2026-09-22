73ed359c38019bfad927f6e70c440f2cd9355a93
Juan Rodriguez <juanblud@udel.edu>
2026-09-22 09:29:33 -0400

    finished the last form component.

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,23 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [selectedChoice, setSelectedChoice] = useState<string>(options[0]);
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Select
+                value={selectedChoice}
+                onChange={(event) => {
+                    setSelectedChoice(event.target.value);
+                }}
+            >
+                {options.map((o) => (
+                    <option key={o} value={o}>
+                        {o}
+                    </option>
+                ))}
+            </Form.Select>
+            <p>{selectedChoice === expectedAnswer ? "✔️" : "❌"}</p>
         </div>
     );
 }
